c01b3b6335b94318d603e1c92e39b0af27313a1f
SMarkus27 <pereira.markus.27@hotmail.com>
2024-04-08 15:09:04 -0300

    chore: add product repository interface

--- src/application/interfaces/repositories/products.py
+++ src/application/interfaces/repositories/products.py
@@ -10,17 +10,17 @@ class IProductsRepository(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    async def get_all_products(self) -> list[Product]:
+    async def find(self) -> list[Product]:
         pass
 
     @abstractmethod
-    async def get_product(self, product_id: str) -> Product:
+    async def find_one_by_id(self, product_id: str) -> Product:
         pass
 
     @abstractmethod
-    async def update_product(self, product_id: str, product: Product) -> Product:
+    async def update(self, product_id: str, product: Product) -> Product:
         pass
 
     @abstractmethod
-    async def delete_product(self, product_id: str) -> None:
+    async def delete(self, product_id: str) -> None:
         pass
