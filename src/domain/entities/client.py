8acc5e15de0f04ccde6528c8e6c1e8e358199920
SMarkus27 <pereira.markus.27@hotmail.com>
2024-04-09 15:43:17 -0300

    chore: client entity

---
+++ src/domain/entities/client.py
@@ -0,0 +1,40 @@
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Client:
+    name: str
+    cpf: str
+    email: str
+
+    def __post_init__(self):
+        Client.verify_name(self.name)
+        Client.verify_cpf(self.cpf)
+        Client.verify_email(self.email)
+
+    @staticmethod
+    def verify_name(name: str) -> str:
+        if len(name) <= 2:
+            raise Exception("Name must be greater than or equal to 1")
+        return name
+
+    @staticmethod
+    def verify_cpf(cpf: str) -> str:
+        if len(cpf) < 11:
+            raise Exception("CPF must have 11 characters")
+        return cpf
+
+    @staticmethod
+    def verify_email(email: str) -> str:
+        regex = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b')
+        valid_email = re.fullmatch(regex, email)
+        if valid_email is None:
+            raise Exception("Email invalid")
+        return email
+
+    @staticmethod
+    def verify_status(status: bool) -> bool:
+        if type(status) is not bool:
+            raise Exception("Status must be greater a boolean")
+        return status
